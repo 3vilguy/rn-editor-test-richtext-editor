34e0ef0150a7af8c5a34cb2ac053530283c2c7fc
3vilguy <2122619+3vilguy@users.noreply.github.com>
2019-08-11 17:43:41 +0100

    Trying to add react native richtext editor.

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+var HBRichTextEditor = require('react-native-richtext-editor');
+var HBToolbar = require('react-native-richtext-editor/HBToolbar');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <HBRichTextEditor
+        ref="myEditor"
+        initialHTML={"bodyForDisplay"} />
+      <HBToolbar />
     </View>
   );
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rn-editor-test-richtext-editor
+
+Demo repo for [richtext-editor](https://github.com/HoneyBook/react-native-richtext-editor).
+Uses [Expo](https://expo.io/).
+
+## To get started:
+```
+yarn
+```
+```
+yarn start
+```
+And now use device or emulator to check the app.
